f987cbfefab74a6e3de67a7793166c00a9f79b48
Niggl0n <nicolai.heilander@yahoo.de>
2023-09-01 20:48:38 +0200

    transcribe podcast batch pipeline;

--- speech_to_text.py
+++ speech_to_text.py
@@ -9,7 +9,12 @@ import librosa
 import numpy as np
 from datasets import Dataset
 
-# Function to load an audio file
+
+#############################################
+# script based on: https://huggingface.co/openai/whisper-large-v2
+#############################################
+
+
 def load_audio_file(file_path):
     return librosa.load(file_path, sr=None)
 
@@ -71,29 +76,44 @@ def create_podcast_transcription(audio_dataset, sr=16000, break_after_n_snippets
         transcription_whisper = transcribe_whisper(audio_snippet, sr, model_str="whisper-base")
         text_snippets.append(transcription_whisper[0])
         if i >= break_after_n_snippets:
+            print("Break for loop.")
             break
     podcast_transcript = ' '.join(text_snippets)
     return podcast_transcript
 
 
 file_paths = ["data/audio/glt1014526399.mp3"]
-segment_length_seconds = 1 * 30
+segment_length_seconds = 5 * 60
 sr = 16000
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 for file_path in file_paths:
     print(file_path)
     filename = os.path.basename(file_path).split('.')[0]
     audio_dataset = create_audio_dataset(file_path, segment_length_seconds=segment_length_seconds)
-    podcast_transcript = create_podcast_transcription(audio_dataset, sr=sr, break_after_n_snippets=2)
-    write_file_path = f"data/transcriptions/{filename}.txt"
-    print(f"Save Transcript to: {write_file_path}")
-    with open(write_file_path, 'w') as file:
-        file.write(podcast_transcript)
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model="openai/whisper-small",
+        chunk_length_s=30,
+        device=device,
+    )
+    for i, audio_snippet in enumerate(audio_dataset):
+        print(f"Transcribe part {i+1} of {str(len(audio_dataset))}.")
+        audio_snippet = np.array(audio_snippet["audio"].copy())
+        podcast_transcript = pipe(audio_snippet, batch_size=8)["text"]
+        write_file_path = f"data/transcriptions/{filename}_{str(i+1)}.txt"
+        print(f"Save Transcript to: {write_file_path}")
+        with open(write_file_path, 'w') as file:
+            file.write(podcast_transcript)
 
 
 # play audio
 # for i in range(5):
 #     sd.play(audio_dataset[i]["audio"], samplerate=sr)  # You might need to adjust the samplerate depending on your audio data
 #     sd.wait()
+
+segment_length_seconds = 5 * 60
+sr = 16000
+audio_dataset = create_audio_dataset(file_path, segment_length_seconds=segment_length_seconds)
 
 
 
